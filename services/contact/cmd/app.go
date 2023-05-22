90428376f4dbd07c3afae98c3252d4dfaccb9db2
buzukbuzuk <75903448+buzukbuzuk@users.noreply.github.com>
2023-05-23 01:07:15 +0600

    Add files via upload

---
+++ services/contact/cmd/app.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"buzukbuzuk.clean-architecture/pkg/store/postgres"
+	"buzukbuzuk.clean-architecture/services/contact/internal/domain"
+)
+
+func main() {
+	dcp := &postgres.DbConnParams{
+		Host:     "localhost",
+		Port:     5432,
+		User:     "user",
+		Password: "Example123+",
+		DbName:   "clean-architecture",
+	}
+
+	db, err := postgres.OpenDB(dcp)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer db.Close()
+
+	ilya := domain.NewContact("Kochemirov", "Ilya", "Alekseevich")
+	anar := domain.NewContact("Daureneva", "Anar", "Alibekkyzy")
+	group1 := domain.NewGroup("Group 1")
+
+	fmt.Println(ilya, anar, group1)
+}
